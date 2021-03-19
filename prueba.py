9a98da7aa2ba24e88886ab19e8d402110c84d058
Jobernal10 <jobernal10@poligran.edu.co>
2021-03-19 11:24:44 -0500

    Cambio random a linea 5

--- prueba.py
+++ prueba.py
@@ -1,3 +1,5 @@
 print ("Hola Mundo, esto es una prueba para la clase de Inenieria de Software")
 print ("Hola Mundo, que tal todo?")
 print ("mama huevo el man que no presenta XD")
+
+print("nuevo cambio a codigo random ASDASDE")
