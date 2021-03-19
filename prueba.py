8a3a6fca7a453eea90d6b45ede99c3edcff52ff0
Jobernal10 <jobernal10@poligran.edu.co>
2021-03-19 11:08:53 -0500

    le cambie 2 linea de codigo a "pluma pluma"

--- prueba.py
+++ prueba.py
@@ -1,3 +1,4 @@
 print ("Hola Mundo, esto es una prueba para la clase de Inenieria de Software")
 print ("Hola Mundo, que tal todo?")
 print ("mama huevo el man que no presenta XD")
+print ("fiesta fiesta, pluma pluma")
