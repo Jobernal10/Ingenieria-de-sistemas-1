5c2cda1fa2428e3ccb7d974394722b38b89082f6
Miguel Angel Garcia Perilla <dreicosworld@gmail.com>
2021-03-22 13:32:31 -0500

    Modificacion final

--- prueba.py
+++ prueba.py
@@ -1,5 +1,4 @@
 print ("Hola Mundo, esto es una prueba para la clase de Inenieria de Software")
 print ("Hola Mundo, que tal todo?")
-print ("mama huevo el man que no presenta XD")
 
-print("nuevo cambio a codigo random ASDASDE")
+print("Nuevo codigo de python :D")
