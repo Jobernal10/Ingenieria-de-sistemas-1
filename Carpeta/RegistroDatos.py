85e897135414d44edb74346beba1378e661dcccc
Jobernal10 <jobernal10@poligran.edu.co>
2021-04-23 11:20:53 -0500

    commit

---
+++ Carpeta/RegistroDatos.py
@@ -0,0 +1,4 @@
+NombreIngreso= ['nombre1','nombre2','nombre3']
+CantidadIngreso= [0.0,0.0,0.0]
+NombreGasto= ['nombre1','nombre2','nombre3']
+CanditadGasto= [0.0,0.0,0.0]
---
+++ Carpeta/Usuario.py
@@ -0,0 +1,8 @@
+Nombre= 'String'
+Apellidos= 'String'
+NumeroDeIdentificacion= 'Int'
+ImagenDeUsuario= 'PNG'
+RegistroPrevisto= 'RegistrosDatos'
+RegistroReal= 'class()'
+RegistosDatos= 'class()'
+Presupuesto= 0.0
