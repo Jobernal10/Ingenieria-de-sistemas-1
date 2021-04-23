b5cd5e3f6ef0e805bb1455456788ab143c075038
Jobernal10 <jobernal10@poligran.edu.co>
2021-04-23 11:36:29 -0500

    commit on 'main'

--- CARPETA DE CLASES/Generar presupuesto compartido.py
+++
@@ -1,4 +0,0 @@
-print ("Hola Mundo, esto es una prueba para la clase de Inenieria de Software")
-print ("Hola Mundo, que tal todo?")
-print ("mama huevo el man que no presenta XD")
-print("mirando CAMBIO")
