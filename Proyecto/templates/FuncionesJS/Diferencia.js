83c9b41ec559b3ec01f78a25209b407aeab2d392
Leonardohy <79870593+Leonardohy@users.noreply.github.com>
2021-04-28 17:09:00 -0500

    .

--- Proyecto/templates/FuncionesJS/Diferencia.js
+++ Proyecto/templates/FuncionesJS/Diferencia.js
@@ -1,6 +1,6 @@
 botonAñadir.onclick = function(){
   var resultado = typeMontoEstimado.value-typeMontoReal.value;
-  alert("esto es algo de "+resultado)
-  var n1 = prompt("Dime un numero");
-  var n2 = prompt("Dime otro numero");
+  alert("esto es algo de "+resultado);
+  var divsaludar=document.getElementById("hola")
+  divsaludar.innerText= ("Total: "+resultado)
 }
