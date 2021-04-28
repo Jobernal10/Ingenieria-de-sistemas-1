60bd37e1d742e819b3cdfb6810bcc15af50c95d1
Leonardohy <79870593+Leonardohy@users.noreply.github.com>
2021-04-28 14:52:48 -0500

    .

--- Proyecto/templates/FuncionesJS/Funcion1.js
+++ Proyecto/templates/FuncionesJS/Funcion1.js
@@ -1,8 +1,4 @@
-
-function Posibilidades(dato1, dato2)
-{
-    var n1 = prompt("Dime un numero");
-    var n2 = prompt("Dime otro numero");
-    document.write("La suma de "+ n1 + " y "+ n2 + " es: ");
-    document.write(parseInt(n1)+parseInt(n2));
-}
+var n1 = prompt("Dime un numero");
+var n2 = prompt("Dime otro numero");
+document.write("La suma de "+ n1 + " y "+ n2 + " es: ");
+document.write(parseInt(n1)+parseInt(n2));
