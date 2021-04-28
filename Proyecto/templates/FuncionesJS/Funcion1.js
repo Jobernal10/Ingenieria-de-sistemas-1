90c6daa8646abf20085f6bb1748209e23297807b
Leonardohy <79870593+Leonardohy@users.noreply.github.com>
2021-04-28 15:51:13 -0500

    .

--- Proyecto/templates/FuncionesJS/Funcion1.js
+++ Proyecto/templates/FuncionesJS/Funcion1.js
@@ -1,4 +1,6 @@
 var n1 = prompt("Dime un numero");
 var n2 = prompt("Dime otro numero");
-document.write("La suma de "+ n1 + " y "+ n2 + " es: ");
-document.write(parseInt(n1)+parseInt(n2));
+function dif (n1,n2){
+    document.write("La suma de "+ n1 + " y "+ n2 + " es: ") + document.write(parseInt(n1)+parseInt(n2));
+   }
+   dif(n1,n2);
