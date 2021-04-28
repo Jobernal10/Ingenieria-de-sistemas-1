f45a8e43a102d86b27f0a934723221b3e0c20119
Jobernal10 <jobernal10@poligran.edu.co>
2021-04-28 16:42:57 -0500

    commit

--- Proyecto/templates/FuncionesJS/Diferencia.js
+++ Proyecto/templates/FuncionesJS/Diferencia.js
@@ -1,4 +1,6 @@
-function diferencia(p,real) {
-    var resultado = preeviso-real;
-    return diferencia;
-  }
+botonAñadir.onclick = function(){
+  var resultado = typeMontoEstimado.value-typeMontoReal.value;
+  alert("esto es algo de "+resultado)
+  var n1 = prompt("Dime un numero");
+  var n2 = prompt("Dime otro numero");
+}
