136d0ff73d5745ede66818f1f6a5dc76f0809444
Leonardohy <79870593+Leonardohy@users.noreply.github.com>
2021-04-29 18:00:30 -0500

    .

--- Proyecto/templates/FuncionesJS/Diferencia.js
+++ Proyecto/templates/FuncionesJS/Diferencia.js
@@ -1,5 +1,16 @@
 botonAñadir1.onclick = function(){
-  var resultado = typeEstimado.value-typeReal.value;
+  var te=typeEstimado.value;
+  var tr=typeReal.value;
+  var resultado=0;
+  if(te>tr){
+     resultado = te-tr;
+  }
+  else if(te<tr){ 
+     resultado = tr-te;
+  } 
+  else if(te==tr){
+    resultado=0;
+  }
   var divsaludar=document.getElementById("hola")
   divsaludar.innerText= ("Total: "+resultado)
 }
