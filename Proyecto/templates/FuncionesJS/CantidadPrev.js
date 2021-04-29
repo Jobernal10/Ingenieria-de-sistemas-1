cab0cc18f11d90aa587ec70715b0bff0b866ebcf
Jobernal10 <jobernal10@poligran.edu.co>
2021-04-28 20:32:18 -0500

    commir

--- Proyecto/templates/FuncionesJS/CantidadPrev.js
+++ Proyecto/templates/FuncionesJS/CantidadPrev.js
@@ -1,16 +1,16 @@
 
 botonAñadir.onclick = function(){
     var tabla=  document.getElementById("tablaPresupuesto");
-    alert("SE ACTIVO ESTA MONDA9: "+ tabla.rows.length)
-    alert(parseInt(tabla.rows[2].cells[3].innerText) + parseInt(tabla.rows[1].cells[3].innerText) )
+    //alert("SE ACTIVO ESTA MONDA9: "+ tabla.rows.length)
+    //alert(parseInt(tabla.rows[2].cells[3].innerText) + parseInt(tabla.rows[1].cells[3].innerText) )
     var suma=0;
     for (let i =  1; i < tabla.rows.length; i++) {
 
-      alert("iteracion "+i+"valor "+ parseInt(tabla.rows[i].cells[3].innerText))
+      //alert("iteracion "+i+"valor "+ parseInt(tabla.rows[i].cells[3].innerText))
       valor= parseInt(tabla.rows[i].cells[3].innerText)
     suma=suma+valor;
     }
-    alert("Total es: "+ suma)
+    //alert("Total es: "+ suma)
     var divsaludar=document.getElementById("Ttabla")
   divsaludar.innerText= ("Total: "+suma)
 }
