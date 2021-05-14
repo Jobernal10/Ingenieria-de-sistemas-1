99889ebb4b13c1d0ec5bd82d6c4894223915a32c
Miguel Angel Garcia Perilla <dreicosworld@gmail.com>
2021-05-14 12:38:01 -0500

    test

--- Proyecto/FuncionesJS/Funcion1.js
+++
@@ -1,6 +0,0 @@
-var n1 = prompt("Dime un numero");
-var n2 = prompt("Dime otro numero");
-function dif (n1,n2){
-    document.write("La suma de "+ n1 + " y "+ n2 + " es: ") + document.write(parseInt(n1)+parseInt(n2));
-   }
-   dif(n1,n2);
